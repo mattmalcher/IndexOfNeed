87a0b56baa6c3d41f6af7bf7af942f85283359c9
Matt Thomas <matthewgthomas@gmail.com>
2018-01-10 15:15:25 +0000

    Added script to batch download IMD indicators

---
+++ Datasets/IMD/Scripts/R/download all IMDs.r
@@ -0,0 +1,7 @@
+##
+## Meta-script to download and process Indices of Multiple Deprivation for all four countries
+##
+source("English IMD.r")
+source("Welsh IMD.r")
+source("Scottish IMD.r")
+source("NI IMD.r")
