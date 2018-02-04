f79831feb78e66f565dd86c47027b823caef1acb
Obi Thompson Sargoni <33906525+obisargoni@users.noreply.github.com>
2018-02-04 17:32:48 +0000

    Changes to how recodes from multiple pages are scraped have broken getting the attendance allowance data. Removing the query while it gets debugged.

--- Datasets/StatXplore/Scripts/Python/get_stat_xplore_data.py
+++ Datasets/StatXplore/Scripts/Python/get_stat_xplore_data.py
@@ -77,25 +77,6 @@ with open(output_directory + 'pip_annotations.txt', 'w') as f:
         f.write(annotation+'\n\n')
 
 
-# Numbers claiming Attendance Allowance by primary condidtion, date and geography
-aa = stat_xplore_table.get_stat_xplore_measure_data(table_headers, 
-                                                    schema_headers, 
-                                                    measure_id = 'str:count:AA_Entitled:V_F_AA_Entitled', 
-                                                    field_ids = [   'str:field:AA_Entitled:F_AA_QTR:DATE_NAME',
-                                                                    'str:field:AA_Entitled:V_F_AA_Entitled:DISABLED',
-                                                                    'str:field:AA_Entitled:V_F_AA_Entitled:COA_CODE'],
-                                                    fields_include_total = 'str:field:AA_Entitled:V_F_AA_Entitled:DISABLED', 
-                                                    df_schema = df_schema, 
-                                                    geog_folder_label = 'Geography (residence-based)',
-                                                    geog_field_label = 'National - Regional - LA - OAs',
-                                                    geog_level_label = 'Local Authority')
-
-aa['data'].to_csv(output_directory + 'attendance_allowance_data.csv', index=False)
-with open(output_directory + 'aa_annotations.txt', 'w') as f:
-    for annotation in aa['annotations'].values():
-        f.write(annotation+'\n\n')
-
-
 # Numbers of national insurance registrations by quarter local authority of residence and broad nationality
 nino = stat_xplore_table.get_stat_xplore_measure_data(  table_headers, 
                                                         schema_headers, 
